05b751801ed0a6cd3b5de0ac2c67ef62dbe0bc0a
Rizwan Ali <rizcube@gmail.com>
2015-05-23 23:14:31 +0100

    added funciton for RGB numbers

--- script_forloop.js
+++ script_forloop.js
@@ -4,10 +4,15 @@ var green;
 var blue;
 var rgbColor;
 
+function getRGB( red, green, blue ) {
+  return Math.floor(Math.random() * 256 )
+}
+
+
 for (i=0; i<10; i+=1) {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
+  red = getRGB();
+  green = getRGB();
+  blue = getRGB();
   rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
   html += '<div style="background-color:' + rgbColor + '"></div>';
 }
