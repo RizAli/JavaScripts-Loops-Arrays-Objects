b4dd1615a42d3d2555605945c23da0c0993e4811
Rizwan Ali <rizcube@gmail.com>
2015-05-23 14:17:21 +0100

    a random number is generated anda computer then  attempts to guess the number

--- README.md
+++ README.md
@@ -16,3 +16,31 @@ while (counter < 100000) {
   counter += 1;
 }
 ```
+
+Closer look to while loop:
+--------------------------
+
+```
+var upper = 10;
+var randomNumber = getRandomNumber(upper);
+var attempts = 0;
+var guess;
+
+
+function getRandomNumber(upper){
+  return Math.floor( Math.random() * upper ) + 1;
+}
+
+while ( guess !== randomNumber) {
+  guess = getRandomNumber(upper);
+  attempts +=1;
+}
+
+document.write("<p> The random number was: " +  randomNumber + "<p>")
+document.write("<p> It took the computer " + attempts + " attempts to get it right. </p>")
+
+```
+
+
+
+
--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
-function randomNumbers(upper){
+var upper = 10;
+var randomNumber = getRandomNumber(upper);
+var attempts = 0;
+var guess;
+
+
+function getRandomNumber(upper){
   return Math.floor( Math.random() * upper ) + 1;
 }
 
-var counter = 0;
-while (counter < 100000) {
-  var randNum = randomNumbers(20);
-  document.write(randNum + " ");
-  counter += 1;
+while ( guess !== randomNumber) {
+  guess = getRandomNumber(upper);
+  attempts +=1;
 }
 
+document.write("<p> The random number was: " +  randomNumber + "<p>")
+document.write("<p> It took the computer " + attempts + " attempts to get it right. </p>")
